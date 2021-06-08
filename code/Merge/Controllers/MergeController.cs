d381d73c822ae732ba2123a1ecf99bf903c14ffd
darrendoyle3710 <82107198+darrendoyle3710@users.noreply.github.com>
2021-06-08 17:28:21 +0100

    services up and running

---
+++ code/Merge/Controllers/MergeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Merge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MergeController : ControllerBase
+    {
+        private IConfiguration Configuration;
+        public MergeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            // var numbersService = $"https://{Configuration["numbersServiceURL"]}/numbers";
+            var coloursService = $"https://localhost:44340/colour";
+            var serviceOneResponseCall = await new HttpClient().GetStringAsync(coloursService);
+            // var lettersService = $"https://{Configuration["lettersServiceURL"]}/letters";
+            var themesService = $"https://localhost:44348/theme";
+            var serviceTwoResponseCall = await new HttpClient().GetStringAsync(themesService);
+            var mergedResponse = $"{serviceOneResponseCall}{serviceTwoResponseCall}";
+            return Ok(mergedResponse);
+        }
+    }
+}
---
+++ code/Theme/Controllers/ThemeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Theme.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ThemeController : ControllerBase
+    {
+
+        private static readonly string[] Themes = new[]
+        {
+            "Sadness","Guilt", "Adventure","Wisdom","Regret"
+        };
+
+        [HttpGet]
+        public string Get()
+        {
+            var rnd = new Random();
+            var returnIndex = rnd.Next(0, 4);
+            return Themes[returnIndex];
+        }
+    }
+}
